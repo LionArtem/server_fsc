4b0724de736ff5a96248ef397cdbc1d03b8ffe71
Grinevich Artem <grinevitch.artemy@yandex.ru>
2023-02-09 08:52:12 +0300

    перенести проект

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="page">
+      <header className="header">
+        <h1 className="header__title">
+          Этот сайт создан что бы облегчить жизнь слесаря.
+        </h1>
+        <div className="header__foto rateit"></div>
       </header>
+      <main>
+        <section className="navigation">
+          <nav>
+            <ul className="navigation__list">
+              <li className="navigation__title">
+                <a className="navigation__list-links" href="">
+                  <h2 className="navigation__text">1-ое отделение.</h2>
+                </a>
+              </li>
+              <li className="navigation__title">
+                <a className="navigation__list-links" href="">
+                  <h2 className="navigation__text">2-ое отделение.</h2>
+                </a>
+              </li>
+              <li className="navigation__title">
+                <a className="navigation__list-links" href="">
+                  <h2 className="navigation__text">3-ие отделение.</h2>
+                </a>
+              </li>
+              <li className="navigation__title">
+                <a className="navigation__list-links">
+                  <h2 className="navigation__text">Шихтовый двор.</h2>
+                </a>
+              </li>
+              <li className="navigation__title">
+                <a className="navigation__list-links" href="">
+                  <h2 className="navigation__text">РММ 1-го отделения.</h2>
+                </a>
+              </li>
+              <li className="navigation__title">
+                <a className="navigation__list-links" href="">
+                  <h2 className="navigation__text">
+                    Участок производства порашковой проволоки.
+                  </h2>
+                </a>
+              </li>
+              <li className="navigation__title">
+                <a className="navigation__list-links" href="">
+                  <h2 className="navigation__text">
+                    Газоочистка 1-го отделения.
+                  </h2>
+                </a>
+              </li>
+              <li className="navigation__title">
+                <a className="navigation__list-links" href="">
+                  <h2 className="navigation__text">РММ 2-го отделения.</h2>
+                </a>
+              </li>
+              <li className="navigation__title">
+                <a className="navigation__list-links" href="">
+                  <h2 className="navigation__text">
+                    Газоочистка 3-го отделения.
+                  </h2>
+                </a>
+              </li>
+              <li className="navigation__title">
+                <a className="navigation__list-links" href="">
+                  <h2 className="navigation__text">
+                    Насосная оборотного цыкла.
+                  </h2>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </section>
+      </main>
+      <footer className="footer">
+        <p className="footer__copyright">Артём Гриневич</p>
+        <nav>
+          <a
+            className="footer__link-vk"
+            href="https://vk.com/id82204125"
+            target="_blank"
+          >
+            (Вконтакте)
+          </a>
+        </nav>
+      </footer>
     </div>
   );
 }
