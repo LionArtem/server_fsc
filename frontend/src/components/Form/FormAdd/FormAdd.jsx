e7870a36a7e24e94f46870239b443666fc15d73b
Grinevich Artem <grinevitch.artemy@yandex.ru>
2023-09-02 16:27:23 +0300

    добавить кнопку назад из компонента form

--- frontend/src/components/Form/FormAdd/FormAdd.jsx
+++ frontend/src/components/Form/FormAdd/FormAdd.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import Style from './FormAdd.module.scss';
+import { useNavigate } from 'react-router-dom';
 
 export default function FormAdd({ placeholder }) {
+  const navigate = useNavigate();
   return (
     <form className={Style.form}>
+      <dev className={Style.clouse} onClick={() => navigate(-1)}></dev>
       <input placeholder={placeholder} />
       <button>добавить</button>
     </form>
