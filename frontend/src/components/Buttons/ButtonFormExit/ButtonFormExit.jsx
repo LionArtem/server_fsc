13ee7c99981d6dabc71dac0f9928f5c75ef5c7af
Grinevich Artem <grinevitch.artemy@yandex.ru>
2023-09-30 08:07:31 +0300

    перенести в компонентс кнопку выхода из формы

--- frontend/src/components/Buttons/ButtonFormExit/ButtonFormExit.jsx
+++ frontend/src/components/Buttons/ButtonFormExit/ButtonFormExit.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Style from './Form.module.scss';
+import Style from './ButtonFormExit.module.scss';
 import { useNavigate } from 'react-router-dom';
-import { killAllStateFormValidetion } from '../../redax/slices/formValidetionSlice';
+import { killAllStateFormValidetion } from '../../../redax/slices/formValidetionSlice';
 import { useDispatch } from 'react-redux';
 
 export default function ButtonExit({ linkExit }) {
--- frontend/src/components/Form/Form.jsx
+++ frontend/src/components/Form/Form.jsx
@@ -4,7 +4,7 @@ import Style from './Form.module.scss';
 import { useSelector } from 'react-redux';
 import { selectformValidetion } from '../../redax/slices/formValidetionSlice';
 import ButtonSubmit from '../Buttons/ButtonSubmit/ButtonSubmit';
-import ButtonExit from './ButtonExit';
+import ButtonExit from '../Buttons/ButtonFormExit/ButtonFormExit';
 
 export default function FormAdd({
   hendleSubmit,
